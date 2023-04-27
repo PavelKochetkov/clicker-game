ea43f4408426ef58e592cd66b448714b23a5f324
Pavel Kochetkov <kochetkov_p@mail.ru>
2023-04-28 00:13:04 +0300

    use React Context

--- src/Components/AppContent.js
+++ src/Components/AppContent.js
@@ -1,41 +1,10 @@
-import React, { useRef } from 'react'
+import React, { useContext } from 'react'
 import '../css/AppContent.css'
+import { Context } from '../context'
 
-export default function AppContent(props) {
 
-    //Доступ к DOM- элемантам
-    const game = useRef()
-    const box = useRef()
-    const btn = useRef()
-    //Функция случайных значений   
-    function getRandom(min, max) {
-        return Math.floor(Math.random() * (max - min) + min)
-    }
-    //Функция генерирования квадратов
-    const renderBox = () => {
-        const arrayColors = ['red', 'blue', 'green', 'orange', 'navy', 'purple', 'maroon']
-        const boxSize = getRandom(30, 100)
-        const gameSize = game.current.getBoundingClientRect()
-        const maxTop = gameSize.height - boxSize
-        const maxLeft = gameSize.width - boxSize
-        const randomColorIndex = getRandom(0, arrayColors.length)
-        box.current.style.height = box.current.style.width = boxSize + 'px'
-        box.current.style.position = 'relative'
-        box.current.style.backgroundColor = arrayColors[randomColorIndex]
-        box.current.style.top = getRandom(0, maxTop) + 'px'
-        box.current.style.left = getRandom(0, maxLeft) + 'px'
-        box.current.style.cursor = 'pointer'
-    }
-    //Функция запуска игры
-    const startGame = () => {
-        btn.current.style.display = 'none'
-        renderBox()
-        props.startTime()
-    }
-    //Генерирование квадратов по клику на них
-    function handleBoxClick() {
-       renderBox()
-    }
+export default function AppContent() {
+    const {startGame,handleBoxClick,game,box,btn} = useContext(Context)
     return (
         <div className='app_content'>
             <button ref={btn} onClick={startGame} className="btn">Начать</button>
--- src/Components/AppHeader.js
+++ src/Components/AppHeader.js
@@ -4,17 +4,17 @@ import '../css/AppHeader.css'
 
 
 export default function AppHeader(props) {
-    const {counter} = useContext(Context)
+    const {counter, hide, time, result, startTime} = useContext(Context)
     
     return (
         <div className="app_header">
-            <h1 id='time' ref={props.hide}>
+            <h1 id='time' ref={hide}>
                 Время игры: 
-                <span ref={props.time} onClick={props.startTime}>
+                <span ref={time} onClick={startTime}>
                     {counter}
                 </span>
             </h1>
-            <h1 className="hide" ref={props.result}>Ваш результат: ЗДАРОВА, ЗАЕБАЛ <span></span></h1>
+            <h1 className="hide" ref={result}>Ваш результат: Результат <span></span></h1>
         </div>
     )
 }
